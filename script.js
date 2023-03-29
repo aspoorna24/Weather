2a3d7d12e4bc99148cac3a1b866c4a6f0ae94304
Poorna <69906063+aspoorna24@users.noreply.github.com>
2023-03-29 16:19:42 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -33,17 +33,18 @@ submit.addEventListener("click", (e) => {
 
 getWeather("Delhi")
 
-
+ /*
+const cp=[];
 
 var text = "<tr>";
 const getWeathers = (city1) => {
 		text += "<td>" + city1 + "</td>";
-	fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city1, options)
+	fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+city1, options)
 		.then(response => response.json())
 		.then((response) => {
 			console.log(response)
-			a = response.cloud_pct
-			b = response.temp
+			cp.push(response.cloud_pct);
+			/*b = response.temp
 			c = response.feels_like
 			d = response.humidity
 			e = response.min_temp
@@ -55,11 +56,13 @@ const getWeathers = (city1) => {
 			
 		   
 		})
-    
+        
 		.catch(err => console.error(err));
 		
-	text += "</tr>";
-	console.log(text)
+	
+	console.log(text);
+	
+
 }
 
 
@@ -67,11 +70,13 @@ const places = ["Boston", "Lucknow", "Kolkata", "Chennai"];
 for (let i = 0; i < places.length; i++) {
 	getWeathers(places[i])
 	
+	console.log(cp[i]);
+	text += "<td>"+cp[i]+"<td>";
+	text += "</tr>";
 	document.getElementById("demo").innerHTML = text;
 	
 }
 
- /*
 
 const getWeathers = (city1) => {
 	fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city1, options)
@@ -97,4 +102,45 @@ const getWeathers = (city1) => {
 const places = ["Boston"];
 for (let j = 0; j < places.length; j++) {
 	getWeathers(places[j])
-}*/
+} */
+
+
+//const places = ["Boston", "Lucknow", "Kolkata", "Chennai"];
+//const cp=[];
+
+	fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Boston', options)
+		.then(response => response.json())
+		.then((response) => {
+			console.log(response)
+			cloud_pct1.innerHTML = response.cloud_pct
+			temp1.innerHTML = response.temp
+			feels_like1.innerHTML = response.feels_like
+			humidity1.innerHTML = response.humidity
+			min_temp1.innerHTML = response.min_temp
+			max_temp1.innerHTML = response.max_temp
+			wind_speed1.innerHTML = response.wind_speed
+			wind_degrees1.innerHTML = response.wind_degrees
+			sunrise1.innerHTML = response.sunrise
+			sunset1.innerHTML = response.sunset
+		})
+		.catch(err => console.error(err));
+		
+
+		fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Lucknow', options)
+		.then(response => response.json())
+		.then((response) => {
+			console.log(response)
+			cloud_pct2.innerHTML = response.cloud_pct
+			temp2.innerHTML = response.temp
+			feels_like2.innerHTML = response.feels_like
+			humidity2.innerHTML = response.humidity
+			min_temp2.innerHTML = response.min_temp
+			max_temp2.innerHTML = response.max_temp
+			wind_speed2.innerHTML = response.wind_speed
+			wind_degrees2.innerHTML = response.wind_degrees
+			sunrise2.innerHTML = response.sunrise
+			sunset2.innerHTML = response.sunset
+		})
+		.catch(err => console.error(err));
+		
+
